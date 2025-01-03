5bcd04625037af8885e7e97bc526f84cc6bc5081
mirshad <mirshadkvx@gmail.com>
2025-01-03 11:12:28 -0800

    basic linked list added

---
+++ Linked/structure.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def append(self, data):
+        new_node = Node(data)
+        
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            
+    def display(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data,end=' ')
+            current_node = current_node.next
+            
+            
+            
+
+List = Linked_List()
+List.append(100)
+List.append(1)
+List.append(2)
+List.append(3)
+List.append(10)
+List.append(80)
+List.display()
