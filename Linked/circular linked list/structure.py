03027dc35630d9ccbbceb9f7f862782989523686
mirshad <mirshadkvx@gmail.com>
2025-01-04 11:58:41 -0800

    + circular linked list

--- Linked/circular linked list/structure.py
+++ Linked/circular linked list/structure.py
@@ -4,4 +4,41 @@ class Node:
         self.next = None
         
 
-class circulart 
+class circular_linked_list:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def append(self, data):
+        new_node = Node(data)
+        
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = self.head
+        
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            self.tail.next = self.head
+    
+    def display(self):
+        current = self.head
+        
+        while True:
+            print(current.data, ' ')
+            current = current.next
+            if current == self.head:
+                break
+        
+        
+cir = circular_linked_list()
+
+cir.append(10)
+cir.append(20)
+cir.append(30)
+cir.append(40)
+
+print("Circular Linked List:")
+cir.display()
+
