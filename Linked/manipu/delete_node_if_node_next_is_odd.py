d9de73db597b09390acc7cf42c6b3357f0119edb
mirshad <mirshadkvx@gmail.com>
2025-01-06 11:31:55 -0800

    + delete node if next is odd

---
+++ Linked/manipu/delete_node_if_node_next_is_odd.py
@@ -0,0 +1,58 @@
+'''
+delete node if next is odd
+'''
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+class Lined_list:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def append(self, data):
+        new_node = Node(data)
+        
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data,end=" ")
+            current = current.next
+        print()
+            
+    def delete(self):
+        current = self.head
+        prev = None
+        
+        while current:
+            if current.next and current.next.data % 2 == 1:
+                # print(current.next.data)
+                if prev is None:
+                    self.head = current.next
+                else:
+                    prev.next = current.next
+                current = current.next
+            else:
+                prev = current
+                current = current.next
+                
+                
+List = Lined_list()
+List.append(100)
+List.append(1)
+List.append(2)
+List.append(3)
+List.append(10)
+List.append(80)
+List.display()
+List.delete()
+List.display()
