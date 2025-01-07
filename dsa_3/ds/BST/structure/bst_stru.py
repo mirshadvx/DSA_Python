4b9cf85e5b944faa5f1a0a28ebc7f28022cdfc12
mirshad <mirshadkvx@gmail.com>
2025-01-07 11:37:43 -0800

    + Binary tree structure

---
+++ dsa_3/ds/BST/structure/bst_stru.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+class Binary_tree:
+    def __init__(self):
+        self.root = None
+        
+
+    def insert(self, data):
+        new_node = Node(data)
+        
+        if self.root is None:
+            self.root = new_node
+        else:
+            self._insert(self.root, new_node)
+    
+    def _insert(self, current , new_node):
+        
+        if new_node.data < current.data:
+            if current.left is None:
+                current.left = new_node
+            else:
+                self._insert(current.left, new_node)
+        else:
+            if current.right is None:
+                current.right = new_node
+            else:
+                self._insert(current.right, new_node)
+                
+    
+bst = Binary_tree()
+bst.insert(50)
+bst.insert(45)
+bst.insert(55)
+bst.insert(30)
+bst.insert(100)
+bst.insert(20)
+bst.insert(75)
